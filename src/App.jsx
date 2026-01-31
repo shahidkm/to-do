d263e19e26039361bbdb167c278a1e8a0754ee52
shahidkm <kmshahid432@gmail.com>
2026-01-31 14:42:44 +0530

    Added more default todo tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import RoutesConfig from './components/Routes';
-import HealthCoachLanding from './components/MinhajTest';
 
 function App() {
   return (
     <div className="App">
-      <HealthCoachLanding />
+      <RoutesConfig />
     </div>
   );
 }
