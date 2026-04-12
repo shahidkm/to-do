c337c5c07007eaf9e45145a631d7890cfda30389
shahidkm <kmshahid432@gmail.com>
2026-04-12 11:17:57 +0530

    Add PWA support: include manifest and meta tags for mobile web app capabilities

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,8 @@
-self.addEventListener('push', (event) => {
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', () => {});
+
+
   const data = event.data?.json() ?? {};
   event.waitUntil(
     self.registration.showNotification(data.title || 'Reminder', {
